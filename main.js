a20cab81e0ae6d13832f56acec321e6e87fef327
Ezra Sowden-Guzman <ezratic@gmail.com>
2015-10-28 15:19:25 -0400

    Add blank SCSS partials

--- main.js
+++ main.js
@@ -5,28 +5,6 @@
 define(function (require, exports, module) {
     "use strict";
 
-    var CommandManager = brackets.getModule("command/CommandManager"),
-        Menus          = brackets.getModule("command/Menus");
-
-
-    // Function to run when the menu item is clicked
-    function handleHelloWorld() {
-        window.alert("HAHAHA THIS PROJECT IS EMPTY LOL JOKES ON YOU");
-    }
-
-
-    // First, register a command - a UI-less object associating an id to a handler
-    var MY_COMMAND_ID = "helloworld.sayhello";   // package-style naming to avoid collisions
-    CommandManager.register("Click me :§", MY_COMMAND_ID, handleHelloWorld);
-
-    // Then create a menu item bound to the command
-    // The label of the menu item is the name we gave the command (see above)
-    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    menu.addMenuItem(MY_COMMAND_ID);
-
-    // We could also add a key binding at the same time:
-    //menu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-W");
-    // (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
+    var test = require("js/addUIElement.js");
     
-    ExtensionUtils.loadStyleSheet("styles.css");
 });
